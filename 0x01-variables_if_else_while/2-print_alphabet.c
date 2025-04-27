5034d93716b2913d1c6349ce463048e7f8125cda
SbonisoZama <sbonisozama535@gmail.com>
2025-04-27 19:11:04 +0200

    Fix: betty coding style syntax(trailing whitespace fix)

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int n;
 
-	 for (n = 97; n <= 122; n++) 
-	 {
+	for (n = 97; n <= 122; n++) 
+	{
               putchar(n);
-    }
-	 putchar('\n');
+        }
+	putchar('\n');
 
 	return (0);
 }
