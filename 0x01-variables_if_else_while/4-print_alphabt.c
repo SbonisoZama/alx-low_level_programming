bf3ac13c0337e3f919d42d878609f2ac64447f71
SbonisoZama <sbonisozama535@gmail.com>
2025-04-27 22:10:42 +0200

    fix: betty coding style

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	 char lower_case;
 	 
-	 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	 for(lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	 {
-		 if (lower_case == 'q' || lower_case == 'e')
+		 if(lower_case == 'q' || lower_case == 'e')
 		 {
 			 continue;
 		 }
