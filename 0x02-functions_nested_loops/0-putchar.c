5330aedde10a86b10779687e84e0076fdd377842
SbonisoZama <sbonisozama535@gmail.com>
2025-07-23 17:02:46 +0200

    prints putchar followed by new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints putchar, followed by a new line
+ *
+ * Retrn: Always 0 (Success)
+ */
+int main(void)
+{
+	printf(" _putchar");
+	printf("\n");
+
+	return (0);
+}
+
