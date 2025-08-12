80427a6dc117b695a7dc50403ffa52f0451f8c8a
root <sbonisozama535@gmail.com>
2025-08-12 06:26:27 -0700

    alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stbdlib.h>
+#include <stdio>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar (alphar);
+		alpha++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
