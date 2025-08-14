e9a29824ba47fc319ccda38e724827e1a4a05d62
root <sbonisozama535@gmail.com>
2025-08-14 12:35:58 -0700

    Fix:error: expected declaration or statement at end of input

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,22 @@
+#include "main.h"
 #include<stdlib.h>
 #include<stdio.h>
-#include "main.h"
 
 /**
- * main - print 10 times the alphabet in lowercase
+ * main - printing x10 the alphabet in lowercase, follwed by a new line
  *
- * Retun: Always 0
+ * Return: Always 0
  */
-
-void print_alphabet_x10(void){
-for(int i = 1;i <= 10; i++){
-	char alpha = 'a';
-	while(alpha <= 'z'){
-	        _putchar(alpha);	
-		alpha++;
+void print_alphabet_x10(void)
+{
+	for (int i = 1;i <= 10; i++)
+	{
+		char alpha = 'a';
+		while (alpha < 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-}	
+}
