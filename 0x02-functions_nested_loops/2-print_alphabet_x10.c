dcadac850e352305cd6d36f5c3b59d9f604e2b50
root <sbonisozama535@gmail.com>
2025-08-14 12:55:21 -0700

    Fix: trailing whitespace

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,11 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;	
-	for (i = 0;i < 10; i++)
+	int i;
+	
+	for (i = 0; i < 10; i++)
 	{
-		char alpha = 'a';
+		char alpha = 'a';		
 		
 		while (alpha <= 'z')
 		{
