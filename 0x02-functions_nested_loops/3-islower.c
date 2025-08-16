b7ca9ce953b7a70baf97cb72bbdd3dc09992ff41
SbonisoZama <sbonisozama535@gmail.com>
2025-08-16 06:33:49 +0200

    Fix: Betty documentation

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,9 +5,9 @@
 /**
  * islower - functionn checks lowercase character
  *
- * Return: Always 0
+ * Return: 1 if lowercase, 0 otherwise
  */
-int _isLower(int c)
+int _islower(int c)
 {
 	return ( c >= 'a' && c <= 'z');
 }
